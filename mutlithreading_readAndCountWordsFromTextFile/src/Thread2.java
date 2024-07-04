c75902907d879be76d500e0796ff5bcf3c967209
Albazthebranded <150098185+Albazthebranded@users.noreply.github.com>
2024-07-04 15:58:13 +0300

    Add files via upload

---
+++ mutlithreading_readAndCountWordsFromTextFile/src/Thread2.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.InvalidPathException;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Thread2 extends Thread {
+
+    @Override
+    public void run() {
+        try {
+
+            String filename = "LOTR_EXCERPT_2.txt";
+            String directory = System.getProperty("user.dir");
+            String path = "";
+            path = directory+ File.separator+filename;
+
+            String line;
+            HashMap<String, Integer> wordMap = new HashMap<>();
+
+            try  {
+                FileReader fileReader = new FileReader(path);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+                while ((line = bufferedReader.readLine()) != null) {
+
+                    Pattern pattern = Pattern.compile("[A-z]+");
+                    Matcher matcher = pattern.matcher(line);
+
+                    while (matcher.find()) {
+                        String word = matcher.group().toLowerCase();
+                        if (wordMap.containsKey(word)) {
+                            wordMap.put(word, wordMap.get(word) + 1);
+                        } else {
+                            wordMap.put(word,1);
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(wordMap);
+
+        } catch (InvalidPathException e) {
+            e.printStackTrace();
+        }
+    }
+}
